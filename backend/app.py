e0afb5132966470622606aeaf73087da0b4b552a
Newton Kiprono <NKiprono@safaricom.co.ke>
2026-01-29 09:40:44 +0300

    feat: add video generation endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Form, File, UploadFile
-from utils.lib import imageSaver
+from fastapi import FastAPI, Form, File, UploadFile, Path
+from utils.lib import image_saver, video_generator
 import uvicorn
 
 app = FastAPI()
@@ -14,10 +14,16 @@ async def upload(
   month: str = Form(...),
   date: int = Form(...),
   generation: int = Form(...),
+  type: str = Form(...),
   image: UploadFile = File(...)
 ):
-  response = await imageSaver(name, month, date, generation, image)
-  return {"response"}
+  response = await image_saver(name, month, date, generation, type, image)
+  return response
+
+@app.get("/generate-video/{month}")
+async def generate_video(month= Path(...)):
+  response = await video_generator(month)
+  return response
 
 if __name__=='__main__':
-  uvicorn.run("app:app", port=8001, reload=False)
+  uvicorn.run("app:app", port=8000, reload=False)
